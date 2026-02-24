f7ca0a27c1487e45fd77ffecfacc1904104eb263
NibeditaRoy2004 <nibeditaroy1124@gmail.com>
2026-02-25 00:06:24 +0530

    Delete src directory

--- src/utils/Mycolor.js
+++
@@ -1,9 +0,0 @@
-export default {
-  primary: "#2E7D32",
-  secondary: "#F9A825",
-  white: "#FFFFFF",
-  lightGreen: "#E8F5E9",
-  green:"#c8e6c9",
-  darkforestgreen:"#2E4E2E",
-  textgreen:"#3d6a3d"
-};
